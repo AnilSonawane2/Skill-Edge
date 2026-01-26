348f2077adf81fed804e04e527760dbef6bbf83f
Anil Sonawane <anil.sonawane2003@gmail.com>
2026-01-26 22:15:03 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,9 @@
 import app from "./app.js";
 const PORT = process.env.PORT;
-import {v2 as cloudinary} from 'cloudinary';
-import Razorpay from "razorpay"; 
 import dotenv from 'dotenv';
-dotenv.config();
-         
-// cloudinary configuration
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
-});
+dotenv.config();         
 
-// razorpay  
-export const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_SECRET
-})
+// for local environment only
 
 app.listen(PORT, () => {
     console.log(`server started at http://localhost:${PORT}`);
